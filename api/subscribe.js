34f304e59806524004229ed0b8df8f303f14b972
Eliav <eliav@eliav-website.com>
2026-02-22 09:03:53 +0200

    debug: add response debug info

--- api/subscribe.js
+++ api/subscribe.js
@@ -58,7 +58,7 @@ module.exports = async function handler(req, res) {
     const data = await response.json();
 
     if (response.ok || response.status === 204) {
-      return res.status(200).json({ success: true });
+      return res.status(200).json({ success: true, debug: { sent: brevoBody, received: data } });
     }
 
     // Contact already exists - that's fine
